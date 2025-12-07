1213e8189d4ea584c665c4db30799169b45357d4
henry <henry.wise0604@gmail.com>
2025-12-07 23:53:29 +0000

    updated main

---
+++ main.js
@@ -0,0 +1,28 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import config from "./config.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`SigmaBot: ${config.edition}`)
+  console.log(`Bot is online | Prefix: ${config.prefix}`);
+});
+
+// Attempt to load features based on config toggles
+
+if (config.features.youtubeChecker) {
+    try {
+        import("./features/YouTube.js").then(mod => mod.default(client, config));
+        console.log("YouTube Checker Active")
+    } catch (err) {
+        console.error("Failed to load YouTube Announcements Script:", err);
+    }
+}
+
+client.login(config.token);
